93d1739646e18f03813a52a932e0f09871460e05
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-12-06 15:41:35 +0530

    My watches MVC

---
+++ Models/WatchDetails.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWatch.Models
+{
+    public class WatchDetails
+    {
+        [Key]
+        public int WatchId { get; set; }
+        [Required]
+        public string? WatchName { get; set; }
+        [Required]
+        public string? WatchModel { get; set; }
+        [Required]
+        public decimal WatchPrice { get; set; }
+        [Required]  
+        public string? WatchColor { get; set;}
+
+
+
+    }
+}
